19fec3a0300e321ff39b4f481152887d3b38c11b
bmohitprasad <bmohitprasad@gmail.com>
2026-04-02 17:57:04 +0530

    Python assignment

--- Python/Assignment/new.py
+++ Python/Assignment/new.py
@@ -1,6 +1,6 @@
-questions = [{'q1': ['o1','o2','o3','o4']}, {'q2': ['o1','o2','o3','o4']}, {'q3': ['o1','o2','o3','o4']}, {'q4': ['o1','o2','o3','o4']}, {'q5': ['o1','o2','o3','o4']}, {'q6': ['o1','o2','o3','o4']}, {'q7': ['o1','o2','o3','o4']}, {'q8': ['o1','o2','o3','o4']}, {'q9': ['o1','o2','o3','o4']}, {'q10': ['o1','o2','o3','o4']}]
+questions = [{'Which of the following is the correct extension of the Python file?': ['.python','.pl','.py','.p']}, {'How is a code block indicated in Python?': ['Brackets','Indentation','Key','None of the above']}, {'What will be the output of the following code snippet?\nprint(type(5 / 2))\nprint(type(5 // 2))': ['float and int','int and float','float and float','int and int']}, {'Which keyword is used to define a function in Python language?': ['Function','def','Fun','Define']}, {'Which of the following data types is used to store values in Key & Value format?': ['List','Tuple','Class','Dictionary']}, {'What will be the output of the expression 4 + 3 % 5?': ['7','2','4','1']}, {'Which of the following loops are not explicitly supported in Python?': ['for','while','do-while','None of the above']}, {'What will be the output of the following code snippet?\na = [1, 2]\nprint(a * 3)': ['Error','[1, 2]','[1, 2, 1, 2]','[1, 2, 1, 2, 1, 2]']}, {'As what data type are the *args stored, when passed into a function?': ['List','Tuple','Dictionary','None of the above']}, {'Which of the following statements is used to create an empty set in Python?': ['( )','[ ]','{ }','set()']}]
 
-answers = ['o1', 'o4', 'o2', 'o3', 'o1', 'o2', 'o4', 'o1', 'o2', 'o3', 'o2']
+answers = ['.py', 'Indentation', 'float and int', 'def', 'Dictionary', '7', 'do-while', '[1, 2, 1, 2, 1, 2]', 'Tuple', 'set()']
 
 c = 0
 marks = 0
@@ -17,6 +17,7 @@ for i in questions:
                 marks += 1
         else:
             print("Enter correct input (1 - 4)!")
+        print("____________________________________________________________________________________________")
 
 
             
