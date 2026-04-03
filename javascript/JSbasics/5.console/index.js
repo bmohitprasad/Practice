a0ec4f94bf760da196da69d8605ef2004dffb8a0
bmohitprasad <bmohitprasad@gmail.com>
2026-04-03 07:30:46 +0530

    js basics

--- javascript/JSbasics/5.console/index.js
+++ javascript/JSbasics/5.console/index.js
@@ -1,1 +1,12 @@
 console.info('%cThis is a styled info message!', 'color: blue; font-size: 16px; font-weight: bold;');
+
+function a() {
+    b()
+}
+function b() {
+    c()
+}
+function c() {
+    console.trace()
+}
+a()
