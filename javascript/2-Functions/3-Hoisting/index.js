5fd880eaa8d6fc347f8e57486f9f7ea8628499b2
bmohitprasad <bmohitprasad@gmail.com>
2026-04-14 18:00:37 +0530

    js learning

--- javascript/2-Functions/3-Hoisting/index.js
+++ javascript/2-Functions/3-Hoisting/index.js
@@ -1,22 +1,22 @@
-// let x
+let x
 
-// console.log(x)
-// let x
-// console.log(x)
-// x = 4
+console.log(x)
+let x1
+console.log(x1)
+x = 4
 
-// console.log(b); // ReferenceError: Cannot access 'b' before initialization
-// let b ;
-// b = 4
+console.log(b); // ReferenceError: Cannot access 'b' before initialization
+let b ;
+b = 4
 
-// a = 10
-// console.log(a)
-// var a 
+a = 10
+console.log(a)
+var a 
 
-// hello(); // TypeError: hello is not a function
-// var hello = function() {
-//     console.log("Hi!");
-// };
+hello(); // TypeError: hello is not a function
+var hello = function() {
+    console.log("Hi!");
+};
 
 for (var i = 0; i < 3; i++) {
     setTimeout(function() {
