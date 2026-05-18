28c19392c00f17dc4c62b3df5fdd285a68edb496
bmohitprasad <bmohitprasad@gmail.com>
2026-05-18 23:22:17 +0530

    dsa java

--- DSA/self/Functional Recursion/hello2.java
+++ DSA/self/Functional Recursion/hello2.java
@@ -3,7 +3,12 @@ public class hello2 {
     public static void main (String [] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int ans = runner(n);
+        System.out.println(ans);
+    }
 
-        System.out.println();
+    public static int runner (int n) {
+        if (n == 1) return 1;
+        return n * runner (n - 1); 
     }
 }
